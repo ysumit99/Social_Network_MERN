52e3d5d084af5c4397ac1634ff110a1099c99370
ysumit99 <ysumit99@gmail.com>
2018-11-04 00:44:04 +0530

    Routes set

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 //DB Config
@@ -15,6 +19,11 @@ mongoose
 //Simple route for testing
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000; //first part for production i.e when deployed  on heroku
 
