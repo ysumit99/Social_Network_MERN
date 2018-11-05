93e4729cc0de6ac2c8b8c0a97e7a426170335c78
ysumit99 <ysumit99@gmail.com>
2018-11-05 06:58:29 +0530

    Creating JWT token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://sumit:Sumit!!!1995@ds143593.mlab.com:43593/devconnector_sumit"
+    "mongodb://sumit:Sumit!!!1995@ds143593.mlab.com:43593/devconnector_sumit",
+  secretOrKey: "secret"
 };
